8deca6854ca502acd7e4cfa4b6b5d53be2a5e566
Esmaeil Fakheri Alamdari <esmail.f.a2000@gmail.com>
2021-02-25 08:00:44 +0330

    Update primenumbers_2.py

--- The Revolution of Prime Numbers/primenumbers_2.py
+++ The Revolution of Prime Numbers/primenumbers_2.py
@@ -1,3 +1,19 @@
+"""
+This is my second prime number project,
+it is not fast and optimized but it will work properly.
+It will find all prime numbers between 2 and 'End'.
+
+1. At first we create an Prime array to
+store 2, 3 and all numbers between 3 and 'End'
+if number is odd and it is not divided by 3
+
+2.we iterate through prime and for each item
+until we reach that item itself,
+we discover multiples of that number
+then remove it from the prime list
+so only prime numbers remain
+"""
+
 def primeNumbers_2(End):
     Prime = [2, 3] + [p for p in range(5, End, 2) if p % 3 != 0]
     P_Length = int(len(Prime) ** 0.5)
