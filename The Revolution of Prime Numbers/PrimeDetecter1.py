ea5b0708135d3975006d30a81d45e7082a9fc9c0
Esmaeil Fakheri Alamdari <esmail.f.a2000@gmail.com>
2021-03-18 17:14:30 +0330

    Update PrimeDetecter1.py

--- The Revolution of Prime Numbers/PrimeDetecter1.py
+++ The Revolution of Prime Numbers/PrimeDetecter1.py
@@ -1,3 +1,18 @@
+# This function checks if the number(num)
+# is a prime number or not
+# At first if number is less than(1) or is one itself,
+# so it is not a prime number
+# Then if it is not 2, 3, 5, it starsts to check some statmentslike:
+# 1.  num - (num // 10) * 10 not in [1, 3, 7, 9]
+# 2.  sum(int(i) for i in str(num)) % 3 == 0
+# (sum of the numbers in the integer can not be devided by 3)
+# then at last we create an iteration of prime numbers untill that number
+# an if the number is one of them it is a prime number
+# or if it is devided by one of the primes so it is not a prime number
+
+# I could actually do this last part first
+# but i decided to add some statements in case that
+# we can find it without the iterations and have least compexity.
 
 def primeDetecter(num):
     num = int(num)
